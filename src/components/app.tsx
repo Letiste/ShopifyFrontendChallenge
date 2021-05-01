21e347e9dfefdd4b4344fb6d223c4b012337daf5
Léo Salé <leo.sale72@gmail.com>
2021-05-01 17:37:51 +0200

    first commit

---
+++ src/components/app.tsx
@@ -0,0 +1,11 @@
+import { FunctionalComponent, h } from 'preact';
+
+const App: FunctionalComponent = () => {
+  return (
+    <div id="preact_root">
+      <p>APP</p>
+    </div>
+  );
+};
+
+export default App;
