2bc0208cbaf812f25d10819fe9a5b54c4c337c9e
Léo Salé <leo.sale72@gmail.com>
2021-05-02 11:00:10 +0200

    feat: add movies search

--- src/components/results/index.tsx
+++ src/components/results/index.tsx
@@ -1,16 +1,25 @@
 import { FunctionalComponent, h } from 'preact';
+import { MovieType } from '../../services/movies';
 import { Movie } from '../movie';
 import style from './style.css';
 
-export const Results: FunctionalComponent = () => {
+export const Results: FunctionalComponent<{
+  movies: MovieType[];
+  search: string;
+}> = ({ movies, search }) => {
   return (
     <div class={style.container}>
-      <h2>Results for "ram"</h2>
-      <ul>
-        <Movie title="Rambo" date={1999} buttonName="Nominate" />
-        <Movie title="Rambo" date={1999} buttonName="Nominate" />
-        <Movie title="Rambo" date={1999} buttonName="Nominate" />
-      </ul>
+      <h2>Results for "{search}"</h2>
+      {movies.length > 0 && (
+        <ul>
+          {movies.map((movie) => {
+            console.log(movies);
+            return (
+              <Movie movie={movie} buttonName="Nominate" key={movie.imdbID} />
+            );
+          })}
+        </ul>
+      )}
     </div>
   );
 };
