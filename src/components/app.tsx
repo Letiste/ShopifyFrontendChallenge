92c2fc5230d273fe92afac0063a987f16a6a1ea8
Léo Salé <leo.sale72@gmail.com>
2021-05-02 12:40:05 +0200

    feat: can add/remove nominated movies (#5)

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -11,13 +11,34 @@ const App: FunctionalComponent = () => {
   const [search, setSearch] = useState('');
   const [movies, setMovies] = useState(Array<MovieType>());
   const [nominates, setNominates] = useState(Array<MovieType>());
+
+  function nominateMovie(movie: MovieType): void {
+    const newNominates = [...nominates, movie];
+    setNominates(newNominates);
+  }
+
+  function removeNominatedMovie(movie: MovieType): void {
+    const newNominates = nominates.filter(
+      (nominate) => nominate.imdbID !== movie.imdbID
+    );
+    setNominates(newNominates);
+  }
+
   return (
     <div id="preact_root">
       <Header />
       <SearchBar setMovies={setMovies} search={search} setSearch={setSearch} />
       <div class="row">
-        <Results movies={movies} search={search} />
-        <Nominations nominates={nominates} />
+        <Results
+          movies={movies}
+          nominates={nominates}
+          search={search}
+          nominateMovie={nominateMovie}
+        />
+        <Nominations
+          nominates={nominates}
+          removeNominatedMovie={removeNominatedMovie}
+        />
       </div>
     </div>
   );
--- src/components/results/index.tsx
+++ src/components/results/index.tsx
@@ -5,17 +5,28 @@ import style from './style.css';
 
 export const Results: FunctionalComponent<{
   movies: MovieType[];
+  nominates: MovieType[];
   search: string;
-}> = ({ movies, search }) => {
+  nominateMovie: (movie: MovieType) => void;
+}> = ({ movies, search, nominateMovie, nominates }) => {
+  console.log('rebuilt');
   return (
     <div class={style.container}>
       <h2>Results for "{search}"</h2>
       {movies.length > 0 && (
         <ul>
           {movies.map((movie) => {
-            console.log(movies);
+            const isDisabled = nominates.some(
+              (nominate) => nominate.imdbID === movie.imdbID
+            );
             return (
-              <Movie movie={movie} buttonName="Nominate" key={movie.imdbID} />
+              <Movie
+                movie={movie}
+                buttonName="Nominate"
+                key={movie.imdbID}
+                handleClick={nominateMovie}
+                isDisabled={isDisabled}
+              />
             );
           })}
         </ul>
